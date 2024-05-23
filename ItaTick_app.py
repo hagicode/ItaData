f2afa87efa3a48c5734b2fc41409e014dedb4548
hagicode <131940602+hagicode@users.noreply.github.com>
2024-05-23 21:35:13 +0900

    Update ItaTick_app.py

--- ItaTick_app.py
+++ ItaTick_app.py
@@ -15,13 +15,13 @@ import os
 st.set_page_config(layout="wide")
 
 l2 = sorted(glob.glob('files/*.parquet', recursive=True))
+st.write(l2)
+# code = st.text_input("銘柄コード","1301")
+# l_in = [s for s in l2 if code in s][0]
 
-code = st.text_input("銘柄コード","1301")
-l_in = [s for s in l2 if code in s][0]
-
-p = pathlib.Path(l_in)
-df = pd.read_parquet(p)
-st.dataframe
+# p = pathlib.Path(l_in)
+# df = pd.read_parquet(p)
+# st.dataframe
 
 #update_date = os.path.split(p)[1].replace("_df_dayIta_all.parquet","")
 #st.write("データ更新日：" + update_date)
@@ -30,11 +30,11 @@ st.dataframe
 # df = pd.read_parquet(p)
 # #df_Ita = df.loc["1301"].loc["2024-05-22 09:50:00"]
 
-col1,col2,col3,col4,col5,col6 = st.columns(6)
-col1.dataframe(df.loc["2024-05-22 09:00:00"])
-col2.dataframe(df.loc["2024-05-22 09:05:00"])
-col3.dataframe(df.loc["2024-05-22 09:10:00"])
-col4.dataframe(df.loc["2024-05-22 09:15:00"])
-col5.dataframe(df.loc["2024-05-22 09:20:00"])
-col6.dataframe(df.loc["2024-05-22 09:25:00"])
+# col1,col2,col3,col4,col5,col6 = st.columns(6)
+# col1.dataframe(df.loc["2024-05-22 09:00:00"])
+# col2.dataframe(df.loc["2024-05-22 09:05:00"])
+# col3.dataframe(df.loc["2024-05-22 09:10:00"])
+# col4.dataframe(df.loc["2024-05-22 09:15:00"])
+# col5.dataframe(df.loc["2024-05-22 09:20:00"])
+# col6.dataframe(df.loc["2024-05-22 09:25:00"])
 
