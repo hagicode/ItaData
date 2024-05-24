df16ee0cfaf03a2e48b8537cabd8525cb253e269
hagicode <131940602+hagicode@users.noreply.github.com>
2024-05-24 23:42:59 +0900

    Add files via upload

--- ItaTick_app.py
+++ ItaTick_app.py
@@ -35,35 +35,35 @@ df_jpx = df_jpx.iloc[:, [1, 2, 3, 9]]
 database = df_jpx[df_jpx["市場・商品区分"] != "ETF・ETN"]
 database_org = database.astype(str)
 
-col1_,col2_ = st.columns(2)
+
 DB_serch = database_org.copy()
 DB_serch["銘柄名"] = [format_text(txt).casefold() for txt in DB_serch["銘柄名"]]
 
-with st.form(key='form1'):
-    with col1_:
-        st.cache_data.clear()
 
-        input_txt = st.text_input('銘柄コードを入力 or 部分一致の検索', '8058')
-        format_input = format_text(input_txt).casefold()
-        
-        DB_result = database[(DB_serch['コード'].str.contains(str(format_input)))|(DB_serch['銘柄名'].str.contains(str(format_input)))]
-        db_result_org = database_org.loc[DB_result.index]
-        st.table(db_result_org)
-        if len(db_result_org) == 0:
-            st.write("一致する銘柄はありません")
-        if len(db_result_org) > 1:
-            st.write("1銘柄になるように入力してください")
-        if len(db_result_org) == 1:
-            symbols = db_result_org.iloc[0,0]
-            name = db_result_org.iloc[0,1]
-            st.write(f"[・TradingViewで開く](https://jp.tradingview.com/chart/?symbol=TSE%3A{symbols})")
-            st.write(f"[・株探で確認する](https://kabutan.jp/stock/chart?code={symbols})")
+col1_,col2_ = st.columns(2)
+with col1_:
+    st.cache_data.clear()
+
+    input_txt = st.text_input('銘柄コードを入力 or 部分一致の検索', '8058')
+    format_input = format_text(input_txt).casefold()
+    
+    DB_result = database[(DB_serch['コード'].str.contains(str(format_input)))|(DB_serch['銘柄名'].str.contains(str(format_input)))]
+    db_result_org = database_org.loc[DB_result.index]
+    st.table(db_result_org)
+    if len(db_result_org) == 0:
+        st.write("一致する銘柄はありません")
+    if len(db_result_org) > 1:
+        st.write("1銘柄になるように入力してください")
+    if len(db_result_org) == 1:
+        symbols = db_result_org.iloc[0,0]
+        name = db_result_org.iloc[0,1]
+        st.write(f"[・TradingViewで開く](https://jp.tradingview.com/chart/?symbol=TSE%3A{symbols})")
+        st.write(f"[・株探で確認する](https://kabutan.jp/stock/chart?code={symbols})")
     code = symbols
 
-    with col2_:
-        # 文字列を日付と時間に分割
-        date_str = st.text_input("日付(yymmdd)","240522")
-        submit = st.form_submit_button("実行")
+with col2_:
+    # 文字列を日付と時間に分割
+    date_str = st.text_input("日付(yymmdd)","240522")
 
 # 日付と時間を適切な形式に変換
 date = datetime.strptime(date_str, '%y%m%d').date()
